49482f4cfbca9ab63cb9a21cac25bf51b5668bda
Dani Prados <daniprados@gmail.com>
2023-01-02 19:25:57 +0100

    Relationship between user and task

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    /**
+     * Get the user that owns the task.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
